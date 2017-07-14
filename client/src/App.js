09f8b2212b813cd1cdb6b508f6fee4bcd58668b1
Eric Constantinides <eric@ericconstantinides.com>
2017-07-13 20:41:15 -0700

    Front-end working with dummy data in moving of react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,68 @@
 // import React, { Component } from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './css/bootstrap.min.css';
+import './css/bootstrap-theme.min.css';
+import './App.css';
 
 import React, { Component } from 'react'
 // import ReactDOM from 'react-dom'
 
 import ListItems from './components/list-items.js'
 import AddItem from './components/add-item.js'
+
+const tempTodos =[
+  {
+      "_id" : "594382b3a210b05693bfd06b",
+      "title" : "integrate a Webpack build",
+      "status" : "open",
+      "__v" : 0
+  },{
+      "_id" : "594382bba210b05693bfd06c",
+      "title" : "Function out the front-end more",
+      "status" : "open",
+      "__v" : 0
+  },{
+      "_id" : "594382c2a210b05693bfd06d",
+      "title" : "add a readme.md",
+      "status" : "open",
+      "__v" : 0
+  },{
+      "_id" : "594382c8a210b05693bfd06e",
+      "title" : "Make this work on a server",
+      "status" : "open",
+      "__v" : 0
+  },{
+      "_id" : "594382d1a210b05693bfd06f",
+      "title" : "Make it work on a per-user basis",
+      "status" : "open",
+      "__v" : 0
+  },{
+      "_id" : "594382dca210b05693bfd070",
+      "title" : "Add a better fetch with promises",
+      "status" : "complete",
+      "__v" : 0
+  },{
+      "_id" : "59438a21a210b05693bfd071",
+      "title" : "Convert this into React",
+      "status" : "open",
+      "__v" : 0
+  },{
+      "_id" : "59438f74a210b05693bfd073",
+      "title" : "I could have a collection which has an ordered array of todo _ids",
+      "status" : "open",
+      "__v" : 0
+  },{
+      "_id" : "59611e18b5963993a95fd869",
+      "title" : "move this into a new repo for React",
+      "status" : "open",
+      "__v" : 0
+  },{
+      "_id" : "59648e3a70ddb90c59b859cb",
+      "title" : "Add a sort to this",
+      "status" : "open",
+      "__v" : 0
+  }
+]
 
 const TODOS_FOLDER = '/todos'
 
@@ -63,7 +119,8 @@ class App extends Component {
     this.setState({todos})
   }
   componentDidMount = () => {
-    this.api.get(data => this.setState({todos: data}))
+    // this.api.get(data => this.setState({todos: data}))
+    this.setState({todos: tempTodos})
   }
   // DB Events:
   api = {
