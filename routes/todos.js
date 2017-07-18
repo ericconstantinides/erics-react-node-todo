9151c01b6c97b19b7407b2a811d602ecbbf822e0
Eric Constantinides <eric@ericconstantinides.com>
2017-07-17 18:15:06 -0700

    removing semicolons

--- routes/todos.js
+++ routes/todos.js
@@ -1,57 +1,57 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-var Todo = require('../models/Todo.js');
+var Todo = require('../models/Todo.js')
 
 /* GET /todos listing. */
 router.get('/', function(req, res, next) {
   Todo.find(function (err, todos) {
-    if (err) return next(err);
-    res.json(todos);
-  });
-});
+    if (err) return next(err)
+    res.json(todos)
+  })
+})
 
 /* POST /todos */
 router.post('/', function(req, res, next) {
   Todo.create(req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
+    if (err) return next(err)
+    res.json(post)
+  })
+})
 
 /* GET /todos/id */
 router.get('/:id', function(req, res, next) {
   Todo.findById(req.params.id, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
+    if (err) return next(err)
+    res.json(post)
+  })
+})
 
 /* BATCH PUT /todos/ */
 router.put('/', function(req, res, next) {
   res.json(req.body.map(todo => {
     const {_id, title, status, order} = todo
     Todo.findByIdAndUpdate(_id, {title, status, order}, (err, post) => {
-      if (err) return next(err);
+      if (err) return next(err)
       return post
-    });
+    })
   }))
-});
+})
 
 /* PUT /todos/:id */
 router.put('/:id', function(req, res, next) {
   Todo.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
+    if (err) return next(err)
+    res.json(post)
+  })
+})
 
 /* DELETE /todos/:id */
 router.delete('/:id', function(req, res, next) {
   Todo.findByIdAndRemove(req.params.id, req.body, function (err, post) {
-    if (err) return next(err);
-    res.json(post);
-  });
-});
+    if (err) return next(err)
+    res.json(post)
+  })
+})
 
-module.exports = router;
+module.exports = router
